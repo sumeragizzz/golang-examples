83c9dfd4d1d18dc3461dbb3789eefc051dfb8297
sumeragizzz <developer@sumeragizzz.dev>
2025-11-27 23:39:06 +0900

    Implementation add task

--- task/cmd/task/main.go
+++ task/cmd/task/main.go
@@ -9,43 +9,58 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Printf("Invalid parameter\n")
+		fmt.Printf("invalid parameter. args: %v\n", os.Args)
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
 	case "list":
-		if tasks, err := task.List("store.json"); err != nil {
-			panic(err)
-		} else {
-			for _, t := range tasks {
-				fmt.Printf("id: %d, content: %s, status: %s\n", t.Id, t.Content, t.Status)
-			}
+		listCommand := flag.NewFlagSet("list", flag.ExitOnError)
+		store := listCommand.String("store", "store.json", "store file path")
+		listCommand.Usage = func() {
+			fmt.Printf("Usage: task list [optoins]\n")
+			fmt.Printf("Options:\n")
+			listCommand.PrintDefaults()
+		}
+		listCommand.Parse(os.Args[2:])
+
+		tasks, err := task.List(*store)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+		for _, t := range tasks {
+			fmt.Printf("%+v\n", t)
 		}
 
 	case "add":
 		addCommand := flag.NewFlagSet("add", flag.ExitOnError)
-		status := addCommand.String("status", "todo", "status(ex: todo, doing, done)")
+		store := addCommand.String("store", "store.json", "store file path")
 		addCommand.Usage = func() {
 			fmt.Printf("Usage: task add [optoins] <content>\n")
 			fmt.Printf("Options:\n")
 			addCommand.PrintDefaults()
 		}
-
 		addCommand.Parse(os.Args[2:])
 		if len(addCommand.Args()) < 1 {
-			fmt.Println("タスク内容は必須です。")
+			fmt.Println("content is required")
 			addCommand.Usage()
 			os.Exit(1)
 		}
 		content := addCommand.Args()[0]
-		fmt.Printf("add: content: %s, status: %s\n", content, *status)
+
+		t, err := task.Add(*store, content)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%+v\n", t)
 
 	case "update":
 	case "delete":
 	case "done":
 	default:
-		fmt.Printf("コマンドが不正です。 %s\n", os.Args[1])
+		fmt.Printf("invalid command. command: %s\n", os.Args[1])
 		os.Exit(1)
 	}
 }
--- task/internal/task/task.go
+++ task/internal/task/task.go
@@ -5,56 +5,81 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 )
+
+type Task struct {
+	Id      int64  `json:"id"`
+	Content string `json:"content"`
+	Status  string `json:"status"`
+}
+
+func (t Task) Create(content string) Task {
+	return Task{
+		Id:      time.Now().UnixNano(),
+		Content: content,
+		Status:  "todo",
+	}
+}
 
 func List(storePath string) ([]Task, error) {
 	if storePath == "" {
-		return nil, errors.New("invalid parameter. storePath: blank")
+		return nil, errors.New("invalid parameter. store: blank")
 	}
-	return load(storePath), nil
+
+	return load(storePath)
 }
 
-func Add(storePath string, content string) {
+func Add(storePath string, content string) (Task, error) {
+	if storePath == "" {
+		return Task{}, errors.New("invalid parameter. store: blank")
+	}
+	if content == "" {
+		return Task{}, errors.New("invalid parameter. content: blank")
+	}
+
+	t := Task{}.Create(content)
+
+	tasks, err := load(storePath)
+	_ = err
+
+	tasks = append(tasks, t)
+
+	if err := save(storePath, tasks); err != nil {
+		return Task{}, err
+	}
+
+	return t, nil
 }
 
-func load(storePath string) []Task {
+func load(storePath string) ([]Task, error) {
 	file, err := os.Open(storePath)
 	if err != nil {
-		panic(err)
+		return []Task{}, fmt.Errorf("file open error. store: %s: %w", storePath, err)
 	}
 	defer file.Close()
 
 	var tasks []Task
 	decoder := json.NewDecoder(file)
 	if err := decoder.Decode(&tasks); err != nil {
-		panic(err)
+		return []Task{}, fmt.Errorf("json decode error. store: %s: %w", storePath, err)
 	}
 
-	return tasks
+	return tasks, nil
 }
 
-func save(storePath string, tasks []Task) {
-
-}
-
-type Task struct {
-	Id      int    `json:"id"`
-	Content string `json:"content"`
-	Status  string `json:"status"`
-}
-
-func (t Task) Create(content string) {
-	var task Task = Task{
-		Id:      1,
-		Content: content,
-		Status:  "todo",
-	}
-
-	data, err := json.Marshal(task)
+func save(storePath string, tasks []Task) error {
+	file, err := os.Create(storePath)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("file open error. store: %s: %w", storePath, err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(tasks); err != nil {
+		return fmt.Errorf("json encode error. tasks: %+v: %w", tasks, err)
 	}
 
-	// TODO JSONファイルに保存
-	fmt.Println(string(data))
+	return nil
 }
