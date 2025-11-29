e735bac358bf289fece221fec1e57f5b75cd14d8
sumeragizzz <developer@sumeragizzz.dev>
2025-11-29 10:09:44 +0900

    Rename variable

--- task/internal/task/task.go
+++ task/internal/task/task.go
@@ -22,16 +22,16 @@ func (t Task) Create(content string) Task {
 	}
 }
 
-func List(storePath string) ([]Task, error) {
-	if storePath == "" {
+func List(store string) ([]Task, error) {
+	if store == "" {
 		return nil, errors.New("invalid parameter. store: blank")
 	}
 
-	return load(storePath)
+	return load(store)
 }
 
-func Add(storePath string, content string) (Task, error) {
-	if storePath == "" {
+func Add(store string, content string) (Task, error) {
+	if store == "" {
 		return Task{}, errors.New("invalid parameter. store: blank")
 	}
 	if content == "" {
@@ -40,20 +40,20 @@ func Add(storePath string, content string) (Task, error) {
 
 	t := Task{}.Create(content)
 
-	tasks, err := load(storePath)
+	tasks, err := load(store)
 	_ = err
 
 	tasks = append(tasks, t)
 
-	if err := save(storePath, tasks); err != nil {
+	if err := save(store, tasks); err != nil {
 		return Task{}, err
 	}
 
 	return t, nil
 }
 
-func Update(storePath string, id int64, content string) (Task, error) {
-	if storePath == "" {
+func Update(store string, id int64, content string) (Task, error) {
+	if store == "" {
 		return Task{}, errors.New("invalid parameter. store: blank")
 	}
 	if id == 0 {
@@ -63,18 +63,18 @@ func Update(storePath string, id int64, content string) (Task, error) {
 		return Task{}, errors.New("invalid parameter. content: blank")
 	}
 
-	return update(storePath, id, content, "")
+	return update(store, id, content, "")
 }
 
-func Delete(storePath string, id int64) (Task, error) {
-	if storePath == "" {
+func Delete(store string, id int64) (Task, error) {
+	if store == "" {
 		return Task{}, errors.New("invalid parameter. store: blank")
 	}
 	if id == 0 {
 		return Task{}, errors.New("invalid parameter. id: zero")
 	}
 
-	tasks, err := load(storePath)
+	tasks, err := load(store)
 	if err != nil {
 		return Task{}, err
 	}
@@ -86,55 +86,55 @@ func Delete(storePath string, id int64) (Task, error) {
 
 	removedTasks := append(tasks[:index], tasks[index+1:]...)
 
-	if err := save(storePath, removedTasks); err != nil {
+	if err := save(store, removedTasks); err != nil {
 		return Task{}, err
 	}
 
 	return t, nil
 }
 
-func Doing(storePath string, id int64) (Task, error) {
-	if storePath == "" {
+func Doing(store string, id int64) (Task, error) {
+	if store == "" {
 		return Task{}, errors.New("invalid parameter. store: blank")
 	}
 	if id == 0 {
 		return Task{}, errors.New("invalid parameter. id: zero")
 	}
 
-	return update(storePath, id, "", "doing")
+	return update(store, id, "", "doing")
 }
 
-func Done(storePath string, id int64) (Task, error) {
-	if storePath == "" {
+func Done(store string, id int64) (Task, error) {
+	if store == "" {
 		return Task{}, errors.New("invalid parameter. store: blank")
 	}
 	if id == 0 {
 		return Task{}, errors.New("invalid parameter. id: zero")
 	}
 
-	return update(storePath, id, "", "done")
+	return update(store, id, "", "done")
 }
 
-func load(storePath string) ([]Task, error) {
-	file, err := os.Open(storePath)
+func load(store string) ([]Task, error) {
+	file, err := os.Open(store)
 	if err != nil {
-		return []Task{}, fmt.Errorf("file open error. store: %s: %w", storePath, err)
+		return []Task{}, fmt.Errorf("file open error. store: %s: %w", store, err)
 	}
 	defer file.Close()
 
 	var tasks []Task
 	decoder := json.NewDecoder(file)
 	if err := decoder.Decode(&tasks); err != nil {
-		return []Task{}, fmt.Errorf("json decode error. store: %s: %w", storePath, err)
+		return []Task{}, fmt.Errorf("json decode error. store: %s: %w", store, err)
 	}
 
 	return tasks, nil
 }
 
-func save(storePath string, tasks []Task) error {
-	file, err := os.Create(storePath)
+func save(store string, tasks []Task) error {
+	file, err := os.Create(store)
 	if err != nil {
-		return fmt.Errorf("file open error. store: %s: %w", storePath, err)
+		return fmt.Errorf("file open error. store: %s: %w", store, err)
 	}
 	defer file.Close()
 
@@ -156,8 +156,8 @@ func search(tasks []Task, id int64) (Task, int, bool) {
 	return Task{}, -1, false
 }
 
-func update(storePath string, id int64, content string, status string) (Task, error) {
-	tasks, err := load(storePath)
+func update(store string, id int64, content string, status string) (Task, error) {
+	tasks, err := load(store)
 	if err != nil {
 		return Task{}, err
 	}
@@ -175,7 +175,7 @@ func update(storePath string, id int64, content string, status string) (Task, er
 	}
 	tasks[index] = t
 
-	if err := save(storePath, tasks); err != nil {
+	if err := save(store, tasks); err != nil {
 		return Task{}, err
 	}
 
